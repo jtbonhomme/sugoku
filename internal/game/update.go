ae4e750e403d0b4bc5e13411ac890810a98fd276
Jean-Thierry BONHOMME <jtbonhomme@gmail.com>
2024-03-28 20:51:58 +0100

    add quit button ESC

--- internal/game/update.go
+++ internal/game/update.go
@@ -1,7 +1,20 @@
 package game
 
+import (
+	"errors"
+	"log"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+var ErrQuit = errors.New("QUIT")
+
 // Update proceeds the game state.
 // Update is called every tick (1/60 [s] by default).
 func (g *Game) Update() error {
+	if ebiten.IsKeyPressed(ebiten.KeyEscape) {
+		log.Printf("key press ESCAPE: exit program")
+		return ErrQuit
+	}
 	return nil
 }
